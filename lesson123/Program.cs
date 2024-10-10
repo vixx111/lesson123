92f7fdea171ba9e6e30938a766a56d6a2e781f8b
Admin <Admin@DESKTOP-UGP0C2G>
2024-10-10 09:52:10 +0200

    Добавьте файлы проекта.

---
+++ lesson123/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите значение y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        double l = 1.51 * Math.Cos(Math.Pow(x, 2)) + 2 * Math.Pow(y, 3);
+        Console.WriteLine($"Значение L равно {l}.");
+    }
+}
